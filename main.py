fix test loss maintaining gradient

main.py
@@ -3,7 +3,7 @@ from model import VitShuffle, Config
 from trainer import Trainer, ImageShuffleDataset
 
 if __name__ == '__main__':
-    config = Config(logging=True)
+    config = Config(logging=False)
 
     # Set up wandb for model performance logging.
     if config.logging:
@@ -19,8 +19,9 @@ if __name__ == '__main__':
     if torch.cuda.is_available():
         device = torch.cuda.current_device()
 
-    data = ImageShuffleDataset(config, directory="./data/", device=device)
     model = VitShuffle(config).to(device)
 
-    trainer = Trainer(model, config, data, None)
+    train = ImageShuffleDataset(config, directory="./data/train/", device=device, n_images=60000)
+    test = ImageShuffleDataset(config, directory="./data/test/", device=device, n_images=10000)
+    trainer = Trainer(model, config, train, test)
     trainer.train()

trainer.py
@@ -11,9 +11,10 @@ from utils import blockshaped
 
 
 class ImageShuffleDataset(Dataset):
-    def __init__(self, config, directory, device):
+    def __init__(self, config, directory, device, n_images):
         super().__init__()
 
+        self.n_images = n_images
         self.config = config
         self.data_directory = directory
         self.device = device
@@ -21,13 +22,13 @@ class ImageShuffleDataset(Dataset):
     def __getitem__(self, idx):
         image = Image.open(os.path.join(self.data_directory, f'{idx}.png'))
         image = ImageOps.grayscale(image)
-        image = to_tensor(image)
+        image = to_tensor(image).squeeze(0)
 
-        b, w, h = image.size()
+        w, h = image.size()
         assert w == h and w % self.config.patch_size == 0
 
         n_patches = int((self.config.image_size / self.config.patch_size) ** 2)
-        blocks = blockshaped(image[0], self.config.patch_size, self.config.patch_size)
+        blocks = blockshaped(image, self.config.patch_size, self.config.patch_size)
         blocks = blocks.view(n_patches, self.config.patch_size**2).contiguous()
 
         shuffled_positions = [i for i in range(0, blocks.size(0))]
@@ -52,19 +53,19 @@ class ImageShuffleDataset(Dataset):
         plt.show()
 
     def __len__(self):
-        return 60000
+        return self.n_images
 
 
 class Trainer:
     def __init__(self, model, config, train_dataset, test_dataset=None):
         self.model = model
         self.config = config
-        self.train_data = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True)
+        self.train_data = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True, drop_last=True)
         self.optimizer = Adam(model.parameters(), lr=config.learning_rate)
 
         self.test_data = None
         if test_dataset:
-            self.test_data = DataLoader(test_dataset, batch_size=config.batch_size, shuffle=True)
+            self.test_data = DataLoader(test_dataset, batch_size=config.batch_size, shuffle=True, drop_last=True)
 
     def train(self):
         self.model.train()
@@ -96,13 +97,14 @@ class Trainer:
         if not self.test_data or len(self.test_data) == 0:
             return
 
-        av_loss = 0
-        for i, (batch_x, batch_y) in enumerate(self.test_data):
-            _, loss = self.model(batch_x, batch_y)
-            av_loss += loss
+        with torch.no_grad():
+            av_loss = 0
+            for i, (batch_x, batch_y) in enumerate(self.test_data):
+                _, loss = self.model(batch_x, batch_y)
+                av_loss += loss
 
-        av_loss /= len(self.test_data)
-        print(f"Average test loss: {av_loss}")
+            av_loss /= len(self.test_data)
+            print(f"Average test loss: {av_loss}")
 
-        if self.config.logging:
-            wandb.log({"test_loss": av_loss})
+            if self.config.logging:
+                wandb.log({"test_loss": av_loss})